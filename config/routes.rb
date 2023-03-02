7cb27c7a31070a7b6dedf424a078731bfd126644
Pablo <pablo.zuniga.vale@gmail.com>
2023-03-02 10:47:44 -0300

    🚧 Add routes and update gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :detail_purcheses
   resources :purchases
     namespace :api do
-      namespace :v1, defaults: { format: :json } do 
+      namespace :v1, defaults: { format: :json } do
         get '/users', action: :users,  controller: :users
         post '/users', action: :create,  controller: :users
         get '/users/:id', action: :show,  controller: :users
@@ -24,7 +24,18 @@ Rails.application.routes.draw do
         post '/users/:user_id/credit_card', action: :create, controller: :credit_cards
         get '/credit_cards', action: :index, controller: :credit_cards
         post '/credit_cards/:id', action: :show, controller: :credit_cards
+
+        # post '/purchase/:user_id/create', action: :create, controller: :purchase
+        post '/invoice/create', action: :create, controller: :invoices
         
+        get '/purchase', action: :index, controller: :purchases
+        post '/purchase/:user_id/create', action: :create, controller: :purchases #Implement
+
+        post '/products/create', action: :create, controller: :products
+        get '/products', action: :index, controller: :products
+
+        post '/categories/create', action: :create, controller: :categories
+        get '/categories', action: :index, controller: :categories
       end
     end
   end    
